a023b72ffc3b7bc7fba10f400d7fcea23b696ebc
Shallowski <shalovskiy.vlad@gmail.com>
2023-04-10 23:12:10 +0200

    add camera control and mousedown function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import TWEEN from "@tweenjs/tween.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
@@ -6,6 +7,7 @@ import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
 import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 
 const globeContainer = document.querySelector(".globe-wrapper");
+let globeRotationStopped = false;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -13,6 +15,13 @@ const camera = new THREE.PerspectiveCamera(
   globeContainer.offsetWidth / globeContainer.offsetHeight,
   0.1,
   1000
+);
+
+scene.background = new THREE.Color(0x111111);
+camera.position.set(
+    22.237025627237497,
+    30.993319159360567,
+    -13.000725315904328
 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -70,7 +79,7 @@ function latLongToVector3(lat, lon, radius, height) {
   return new THREE.Vector3(x, y, z);
 }
 
-function createPointSpheres(points, controls) {
+function createPointSpheres(points) {
   const pointSphereGeometry = new THREE.SphereGeometry(0.3, 20, 20);
   const pointSphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
   const pointSpheres = [];
@@ -83,32 +92,92 @@ function createPointSpheres(points, controls) {
     );
     pointSphere.position.set(position.x, position.y, position.z);
     pointSphere.userData.name = point.name; // add name to user data
-    pointSphere.addEventListener("click", () => {
-      controls.target.copy(pointSphere.position);
-      controls.update();
-    });
     pointSpheres.push(pointSphere);
   }
 
   return pointSpheres;
 }
-scene.background = new THREE.Color(0x111111);
-camera.position.z = 40;
 
 const points = [
   { lat: 40.7128, lon: -74.006, name: "New York" },
   { lat: 51.5074, lon: -0.1278, name: "London" },
   { lat: -33.8688, lon: 151.2093, name: "Sydney" },
-  { lat: 50.27, lon: 30.3124, name: "Kyiv" },
+  { lat: 50.45, lon: 30.5233, name: "Kyiv" },
+  { lat: -33.9188, lon: 18.4233, name: "Cape Town" },
+  { lat: -33.4474, lon: -70.6736, name: "Santiago" },
 ];
 
-const pointSpheres = createPointSpheres(points, controls);
-
-console.log(pointSpheres);
+const pointSpheres = createPointSpheres(points);
 
 for (const pointSphere of pointSpheres) {
   globeWithAtmosphere.add(pointSphere);
 }
+
+// // ===============================camera position======================================
+
+// const lat = 50.27; // latitude of New York
+// const lon = 30.3124; // longitude of New York
+// const radius = 20; // radius of the globe
+// const height = 0.3; // height of the point above the surface of the globe
+
+// // Convert latitude and longitude to a 3D position
+// const position = latLongToVector3(lat, lon, (radius + 20), height);
+
+
+
+// // Set the camera position to the point on the sphere
+// camera.position.copy(position);
+
+// // Set the camera target to the center of the scene
+// controls.target.set(0, 0, 0);
+
+// Update the controls to apply the new settings
+// controls.update();
+
+function onMouseDown(event) {
+  const rect = renderer.domElement.getBoundingClientRect();
+  const left = event.clientX - rect.left;
+  const top = event.clientY - rect.top;
+
+  const x = (left / rect.width) * 2 - 1;
+  const y = -(top / rect.height) * 2 + 1;
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.ray.origin.setFromMatrixPosition(camera.matrixWorld);
+  raycaster.ray.direction
+    .set(x, y, 0.5)
+    .unproject(camera)
+    .sub(raycaster.ray.origin)
+    .normalize();
+
+  const intersects = raycaster.intersectObjects(pointSpheres);
+  if (intersects.length > 0) {
+    const pointSphere = intersects[0].object;
+    const endPosition = pointSphere.position
+      .clone()
+      .add(new THREE.Vector3(0, 0, 5));
+    const startPosition = camera.position.clone();
+    const duration = 2000;
+    console.log(pointSphere);
+
+    new TWEEN.Tween(startPosition)
+      .to(endPosition, duration)
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .onUpdate(() => {
+        camera.position.copy(startPosition);
+        controls.target.copy(pointSphere.position);
+        controls.update();
+      })
+      .start();
+
+    // Stop the rotation of the globe during mouse hover
+    globeRotationStopped = true;
+  }
+}
+
+document.addEventListener("mousedown", onMouseDown);
+
+
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -118,7 +187,10 @@ window.addEventListener("resize", () => {
 
 function animate() {
   requestAnimationFrame(animate);
+  TWEEN.update();
   renderer.render(scene, camera);
-  //   globeWithAtmosphere.rotation.y += 0.001;
+  if (!globeRotationStopped) {
+    globeWithAtmosphere.rotation.y += 0.001;
+  }
 }
 animate();
